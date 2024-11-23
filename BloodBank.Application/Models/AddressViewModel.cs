4a41425d8cc4e0a53d12da148962d727933c45ba
Moisés Macedo <mmacedo.alm@gmail.com>
2024-11-23 18:13:37 -0300

    ♻ refactor: adicionado mais uma propriedade a classe AddressViewModel

--- BloodBank.Application/Models/AddressViewModel.cs
+++ BloodBank.Application/Models/AddressViewModel.cs
@@ -2,15 +2,17 @@ namespace BloodBank.Application.Models
 {
     public class AddressViewModel
     {
-        public AddressViewModel(int addressId, string street, string city, string state, string postalCode)
+        public AddressViewModel(int addressId, int donorId, string street, string city, string state, string postalCode)
         {
             AddressId = addressId;
+            DonorId = donorId;
             Street = street;
             City = city;
             State = state;
             PostalCode = postalCode;
         }
         public int AddressId { get; set; }
+        public int DonorId { get; private set; }
         public string Street { get; private set; }
         public string City { get; private set; }
         public string State { get; set; }

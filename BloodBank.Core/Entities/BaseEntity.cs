33f40d581c18ab9563611caa32beb175dc42a405
Mo3ses <mmacedo.alm@gmail.com>
2024-11-23 10:14:18 -0300

    :sparkles: feat: entidade base criada

---
+++ BloodBank.Core/Entities/BaseEntity.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BloodBank.Core.Entities
+{
+    public abstract class BaseEntity
+    {
+        public BaseEntity()
+        {
+            CreatedAt = DateTime.Now;
+            IsDeleted = false;
+        }
+        public int Id { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+        public bool IsDeleted { get; private set; }
+        public void SetAsDeleted()
+        {
+            IsDeleted = true;
+        }
+
+    }
+}

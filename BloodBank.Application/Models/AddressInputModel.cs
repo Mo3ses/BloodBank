a9890ce203f6d36cfeb9fe2f69a44c6737558ffc
Mo3ses <mmacedo.alm@gmail.com>
2024-11-23 15:26:44 -0300

    :sparkles: feat: construtor adicionado a classe AddressInputModel

--- BloodBank.Application/Models/AddressInputModel.cs
+++ BloodBank.Application/Models/AddressInputModel.cs
@@ -8,6 +8,14 @@ namespace BloodBank.Application.Models
 {
     public class AddressInputModel
     {
+        public AddressInputModel(string street, string city, string state, string postalCode, int donorId)
+        {
+            Street = street;
+            City = City;
+            State = state;
+            PostalCode = PostalCode;
+            DonorId = donorId;
+        }
         public string Street { get; private set; }
         public string City { get; private set; }
         public string State { get; set; }

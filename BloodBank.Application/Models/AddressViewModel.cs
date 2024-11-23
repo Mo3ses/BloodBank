756af0a1d43a59963e575ea3bda9025e4c1ecfb0
Mo3ses <mmacedo.alm@gmail.com>
2024-11-23 15:55:25 -0300

    ♻ refactor: classe AddressViewModel refatorada

--- BloodBank.Application/Models/AddressViewModel.cs
+++ BloodBank.Application/Models/AddressViewModel.cs
@@ -4,19 +4,20 @@ namespace BloodBank.Application.Models
 {
     public class AddressViewModel
     {
-        public AddressViewModel(string street, string city, string state, string postalCode)
+        public AddressViewModel(int addressId, string street, string city, string state, string postalCode)
         {
+            AddressId = addressId;
             Street = street;
             City = city;
             State = state;
             PostalCode = postalCode;
         }
-
+        public int AddressId { get; set; }
         public string Street { get; private set; }
         public string City { get; private set; }
         public string State { get; set; }
         public string PostalCode { get; private set; }
         public static AddressViewModel FromEntity(Address entity)
-        => new AddressViewModel(street: entity.Street, city: entity.City, state: entity.State, postalCode: entity.PostalCode);
+        => new AddressViewModel(addressId: entity.Id, street: entity.Street, city: entity.City, state: entity.State, postalCode: entity.PostalCode);
     }
 }

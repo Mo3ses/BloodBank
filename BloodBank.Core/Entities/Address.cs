ef6b65db048b4dec348f138b0ecdf179caa25d27
Mo3ses <mmacedo.alm@gmail.com>
2024-11-23 12:10:06 -0300

    :recycle: refactor: adicionado propriedade para melhor relacionamento

--- BloodBank.Core/Entities/Address.cs
+++ BloodBank.Core/Entities/Address.cs
@@ -2,14 +2,15 @@ using BloodBank.Core.Entities;
 
 public class Address : BaseEntity
 {
-    public Address(string postalCode, Donor donor) : base()
+    public Address(string postalCode, int donorId) : base()
     {
         PostalCode = postalCode;
-        Donor = donor;
+        DonorId = donorId;
     }
     public string Street { get; private set; }
     public string City { get; private set; }
     public string State { get; set; }
     public string PostalCode { get; private set; }
+    public int DonorId { get; private set; }
     public Donor Donor { get; private set; }
 }

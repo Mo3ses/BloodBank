3d3a5eb0076be3ca38e10bf9f5a2c1947124b205
Mo3ses <mmacedo.alm@gmail.com>
2024-11-23 14:33:01 -0300

    :sparkles: feat: incluido doador na listagem de endereços cadastrados

--- BloodBank.Infrastructure/Persistence/Repositories/AddressRepository.cs
+++ BloodBank.Infrastructure/Persistence/Repositories/AddressRepository.cs
@@ -20,6 +20,7 @@ namespace BloodBank.Infrastructure.Persistence.Repositories
         {
             var results = await _context.Addresses
                 .Where(a => !a.IsDeleted)
+                .Include(a => a.Donor)
                 .ToListAsync();
             return results;
         }
@@ -28,6 +29,7 @@ namespace BloodBank.Infrastructure.Persistence.Repositories
         {
             var result = await _context.Addresses
                 .Where(a => !a.IsDeleted)
+                .Include(a => a.Donor)
                 .SingleOrDefaultAsync(a => a.Id == id);
             return result;
         }

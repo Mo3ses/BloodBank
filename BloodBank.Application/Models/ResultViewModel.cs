a5f9b378e03d3c5364a214bbf3c20708016e695b
Mo3ses <mmacedo.alm@gmail.com>
2024-11-25 12:16:57 -0300

    ✨ feat: classe resultview model para tratamento de exceções

---
+++ BloodBank.Application/Models/ResultViewModel.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BloodBank.Application.Models
+{
+    public class ResultViewModel
+    {
+        public ResultViewModel(bool isSuccess = true, string message = "")
+        {
+            IsSuccess = isSuccess;
+            Message = message;
+        }
+        public bool IsSuccess { get; private set; }
+        public string Message { get; private set; }
+    }
+    public class ResultViewModel<T> : ResultViewModel
+    {
+        public ResultViewModel(T? data, bool isSuccess = true, string message = "") : base(isSuccess, message)
+        {
+            Data = data;
+        }
+        public T? Data { get; private set; }
+    }
+}

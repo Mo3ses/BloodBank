19d9920dddb890bdf3b6e52f3418c72091ad8048
Mo3ses <mmacedo.alm@gmail.com>
2024-11-30 17:25:16 -0300

    ♻ refactor:  removido lista de Donations da classe Donor

--- BloodBank.Application/Models/DonorViewModel.cs
+++ BloodBank.Application/Models/DonorViewModel.cs
@@ -4,7 +4,7 @@ namespace BloodBank.Application.Models
 {
     public class DonorViewModel
     {
-        public DonorViewModel(int donorId, string fullName, string email, DateTime birthDate, string gender, double weight, string bloodType, string rhFactor, List<Address> address, List<Donation> donations)
+        public DonorViewModel(int donorId, string fullName, string email, DateTime birthDate, string gender, double weight, string bloodType, string rhFactor, List<Address> address)
         {
             DonorId = donorId;
             FullName = fullName;
@@ -15,7 +15,6 @@ namespace BloodBank.Application.Models
             BloodType = bloodType;
             RhFactor = rhFactor;
             Addresses = address.Select(a => AddressViewModel.FromEntity(a)).ToList();
-            Donations = donations.Select(d => DonationViewModel.FromEntity(d)).ToList();
         }
         public int DonorId { get; private set; }
         public string FullName { get; private set; }
@@ -26,7 +25,6 @@ namespace BloodBank.Application.Models
         public string BloodType { get; private set; }
         public string RhFactor { get; private set; }
         public List<AddressViewModel> Addresses { get; private set; }
-        public List<DonationViewModel> Donations { get; private set; }
         public static DonorViewModel FromEntity(Donor entity)
         => new(
             donorId: entity.Id,
@@ -37,8 +35,7 @@ namespace BloodBank.Application.Models
             weight: entity.Weight,
             bloodType: entity.BloodType,
             rhFactor: entity.RhFactor,
-            address: entity.Addresses,
-            donations: entity.Donations
+            address: entity.Addresses
             );
     }
 }
